f0d4aa9543e16a20e190dde3a9742e9ebb8a9cbd
Oleg Raskin <raskin.o@yandex.ru>
2020-03-21 02:45:00 +0400

    Project renaming

---
+++ src/test/java/ru/olegraskin/gpregistry/GpRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.olegraskin.gpregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
